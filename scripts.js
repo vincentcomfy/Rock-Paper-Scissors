544c1782bab9c177bec2cec26764c674467ce569
vincent <85801240+vincentcomfy@users.noreply.github.com>
2024-12-04 02:34:00 +0800

    Wrote logic for computer choice

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+// Global values
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+
+// Generate Computer "choice"
+function getComputerChoice() {
+    let numberGenerated = Math.floor(Math.random() * 3) + 1;
+    if (numberGenerated === rock) {
+        return "rock";
+    } else if (numberGenerated === paper) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(`The computer choice is ${getComputerChoice()}`);
